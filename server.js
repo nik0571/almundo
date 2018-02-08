8fcebdeb224cea2c954bdadccae4ed81b3bd1335
Naycool Gonzalez <naycoolgonzalez@gmail.com>
2018-02-07 23:39:59 -0500

    Se cargan librerias y se crea funcion para filtrar los hoteles

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 /**
  * @name SERVER
  * @author Naycool Gonzalez <naycoolgonzalez@gmail.com>
@@ -7,6 +6,10 @@
 
 var http = require('http');
 var path = require('path');
+var fs = require("fs");
+var Helpers = require('./server/helpers/helpers.js');
+var Messages = require('./server/lib/messages.js');
+
 var express = require('express');
 var app = express();
 var server = http.createServer(app);
@@ -15,6 +18,26 @@ app
   .use(express.static('./app/'))
   .get('/*', function (req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
+  })
+  .post('/api/getHotels', function (req, res) {
+    var hotels = JSON.parse(fs.readFileSync("server/models/data.json"));
+    var filter = hotels;
+
+    if(Helpers.emptyDatas(req.query.score) || (req.query.name !== undefined && req.query.name !== "")) {
+      filter = [];
+      hotels.forEach(function (element, index) {
+        var name = (new RegExp(".*" + Helpers.slugify(req.query.name) + ".*", "ig").test(Helpers.slugify(element.name)));
+        var score = ([3,1].indexOf(element.stars) !== -1);
+        if (name || score)
+          filter.push(element);
+      });
+    }
+
+    res.send({
+      data: filter,
+      msj: filter.length > 0 ? Messages.global.success.loadHotels : Messages.global.errors.loadHotels,
+      event: filter.length > 0
+    });
   })
 
 
