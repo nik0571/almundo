f6d782134724842428275353cd760d3af7e6ba9a
Naycool Gonzalez <naycoolgonzalez@gmail.com>
2018-02-08 10:52:29 -0500

    Se crea directiva para imprimer un hotel
    Se crea una directiva para cargar la tarjeta del hotel
    Adirecinalmente se ajusta para cuando se genere un error de cargar la imagen del hotel salga una por derecto

--- app/scripts/directives.js
+++ app/scripts/directives.js
@@ -11,6 +11,7 @@
   angular
   .module('app')
   .directive('mainHeader', [mainHeader])
+  .directive('hotelCard', [hotelCard])
 
 
   // Main Header
@@ -19,6 +20,23 @@
       restric: "EA",
       replace: true,
       templateUrl: "views/layout/header.html" + vApp
+    };
+  }
+
+  //Tarjeta de hotel
+  function hotelCard () {
+    return {
+      restric: "EA",
+      replace: true,
+      templateUrl: "views/directives/hotel-card.html" + vApp,
+      scope: {
+        hotel: "="
+      },
+      link: function (scope, element, attrs) {
+        angular.element(".row.image", element).css({
+          "background-image": "url(images/hotels/"+scope.hotel.image+"), url(images/image-not-found.jpg)"
+        });
+      }
     };
   }
 
